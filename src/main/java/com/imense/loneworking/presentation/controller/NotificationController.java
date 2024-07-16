a4d3a5c633faeb5b9f41d326d89f7471ae78af7b
Akaynadir <akayab.nadir@etu.uae.ac.ma>
2024-07-16 13:46:13 +0100

    notifications CRUD

---
+++ src/main/java/com/imense/loneworking/presentation/controller/NotificationController.java
@@ -0,0 +1,33 @@
+package com.imense.loneworking.presentation.controller;
+
+import com.imense.loneworking.application.dto.Notification.NotificationCreationDto;
+import com.imense.loneworking.application.dto.Notification.NotificationInfoDto;
+import com.imense.loneworking.application.service.serviceInterface.NotificationService;
+import com.imense.loneworking.domain.entity.Notification;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("web/notification")
+    public Notification addNotification(@RequestBody NotificationCreationDto notificationCreationDto){
+        return  notificationService.addNotification(notificationCreationDto);
+    }
+
+    @GetMapping("web/notifications")
+    public List<NotificationInfoDto> getAllNotifications(){
+        return notificationService.getAllNotifications();
+    }
+    @DeleteMapping("web/notification/{id}")
+    public void deleteNotification(@PathVariable Long id){
+        notificationService.deleteNotification(id);
+    }
+}
