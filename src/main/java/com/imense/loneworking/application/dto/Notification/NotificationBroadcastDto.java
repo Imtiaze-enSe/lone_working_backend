eea36a54afd7c706670ed619fe8a2f1b5dde6d10
Mehdi Kaalat <mehdikaalat@gmail.com>
2024-07-22 00:59:49 +0100

    Create NotificationBroadcastDto.java

---
+++ src/main/java/com/imense/loneworking/application/dto/Notification/NotificationBroadcastDto.java
@@ -0,0 +1,19 @@
+package com.imense.loneworking.application.dto.Notification;
+
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class NotificationBroadcastDto {
+    private Long id; // Notification ID
+    private String title;
+    private String message;
+    private String sent_by;
+    private String sent_to;
+    private Long site_id;
+}
