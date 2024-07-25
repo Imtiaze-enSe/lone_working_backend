476ff98ea0f4efc534b3a63f1e444f150e88fb10
Mehdi Kaalat <mehdikaalat@gmail.com>
2024-07-25 01:09:00 +0100

    add stomp method for location

--- src/main/java/com/imense/loneworking/application/dto/Worker/LocationUpdateDto.java
+++ src/main/java/com/imense/loneworking/application/dto/Worker/LocationUpdateDto.java
@@ -8,7 +8,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LocationUpdateDto {
-    private String userEmail;
+    private Long user_id;
+    private Long site_id;
     private double latitude;
     private double longitude;
 }
---
+++ src/main/java/com/imense/loneworking/presentation/controller/LocationWebSocketController.java
@@ -0,0 +1,22 @@
+package com.imense.loneworking.presentation.controller;
+
+import com.imense.loneworking.application.dto.Worker.LocationUpdateDto;
+import com.imense.loneworking.application.service.serviceInterface.LocationService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LocationWebSocketController {
+
+    private final LocationService locationService;
+
+    public LocationWebSocketController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @MessageMapping("/sendLocation")
+    public void handleLocationUpdate(LocationUpdateDto locationUpdate) {
+        System.out.println(locationUpdate);
+        locationService.processLocationUpdate(locationUpdate);
+    }
+}
